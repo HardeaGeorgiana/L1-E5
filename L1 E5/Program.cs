aaa0e8c99de35506696b3384b5839cd221c35312
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-28 11:10:38 +0200

    Add project files.

---
+++ L1 E5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace L1_E5
+{
+    /* E.5 Se citesc doua numere de la tastatura, x,y. 
+           Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+    */
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            Console.WriteLine("Introduceti primul numar");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                Console.WriteLine($"{x},{y}");
+            }
+            else
+            {
+                Console.WriteLine($"{y},{x}");
+            }
+        }
+    }
+}
